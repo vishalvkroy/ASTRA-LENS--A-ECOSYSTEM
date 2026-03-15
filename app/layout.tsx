6c136cb645121f6355eb263621e187c33315a004
vishalvkroy <vishalkumar131981@gmail.com>
2026-03-15 20:16:27 +0530

    favicon_fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      </head>
       <body className={`${inter.className} bg-[#0A0F1E] text-white antialiased`}>
         {children}
       </body>
